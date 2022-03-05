8ea1d2dedb39c8c3b05081ec2162affbff39459e
newbiegao <newbiegao@hotmail.com>
2022-03-05 23:01:25 +0800

    init

---
+++ src/main/java/com/keron/projectdependsload/ProjectDependsLoadApplication.java
@@ -0,0 +1,13 @@
+package com.keron.projectdependsload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDependsLoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDependsLoadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keron/projectdependsload/ProjectDependsLoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keron.projectdependsload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDependsLoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
